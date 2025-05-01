7da05b6b523d769c0618147a5530f86c93d9b6c6
Chad Anderson <Chad.Anderson@vu.com>
2025-05-01 16:13:43 -0500

    pull names, use max and get to determine mode, list and sort values, print mode name and its value

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ The program creates a Python dictionary that maps the sender's mail address to a
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
+
+
+
+
 ```
 
 ## Desired Output
--- counter.py
+++ counter.py
@@ -3,7 +3,20 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
-        
+    counter = dict()
+    for line in handle:
+        line = line.rstrip()
+        if not line.startswith('From '): continue
+        words = line.split()
+        days = words[1]
+        if days not in counter:
+            counter[days] = 1
+        else:
+            counter[days] += 1
+    bigkey = max(counter, key=counter.get)
+    valsort = list(counter.values())
+    bigval = max(valsort)
+    print(bigkey, bigval)
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
